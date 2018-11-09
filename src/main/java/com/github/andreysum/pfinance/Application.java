874bda0817d03d9841c601b95c9d90a43e38c9eb
Sumtsov Andrey <andreysum@gmail.com>
2018-11-09 23:45:54 +0300

    Initial commit

---
+++ src/main/java/com/github/andreysum/pfinance/Application.java
@@ -0,0 +1,10 @@
+package com.github.andreysum.pfinance;
+
+/**
+ * @author andreysum
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
