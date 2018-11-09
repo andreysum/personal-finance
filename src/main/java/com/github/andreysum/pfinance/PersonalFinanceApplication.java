7f0425a5b9fed165ad27c7b45617a7348402d82b
Sumtsov Andrey <andreysum@gmail.com>
2018-11-10 01:04:48 +0300

    Удалены неиспользуемые файлы и каталоги

---
+++ src/main/java/com/github/andreysum/pfinance/PersonalFinanceApplication.java
@@ -0,0 +1,20 @@
+package com.github.andreysum.pfinance;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Главный класс JavaFX приложения.
+ *
+ * @author Andrey Sumtsov
+ */
+public class PersonalFinanceApplication extends Application {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
