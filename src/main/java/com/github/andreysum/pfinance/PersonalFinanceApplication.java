2b85eb170c4bc52561c834380c2e542346df8617
Sumtsov Andrey <andreysum@gmail.com>
2018-11-10 02:37:09 +0300

    * application now executable

--- src/main/java/com/github/andreysum/pfinance/PersonalFinanceApplication.java
+++ src/main/java/com/github/andreysum/pfinance/PersonalFinanceApplication.java
@@ -1,7 +1,16 @@
 package com.github.andreysum.pfinance;
 
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 /**
  * Главный класс JavaFX приложения.
@@ -10,11 +19,26 @@ import javafx.stage.Stage;
  */
 public class PersonalFinanceApplication extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        launch(args);
     }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Spring context initialization
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring/application-context.xml");
 
+
+        // bugfix with fonts blur in linux
+        System.setProperty("prism.lcdtext", "false");
+
+        final FXMLLoader loader = new FXMLLoader();
+        // getting controllers through the Spring context
+        loader.setControllerFactory(ctx::getBean);
+
+        // stage initialization
+        Resource fxmlDefinition = ctx.getResource("javafx/main.fxml");
+        Parent root = loader.load(fxmlDefinition.getInputStream());
+        primaryStage.setTitle("Персональные финансы");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
