8ed2fecbca3b91d74cdd02f7afa0e92836790c47
Andrey Sumtsov <felix2k@tut.by>
2020-02-13 22:42:35 +0300

    + редактирование колонки с названием категории

--- src/main/java/com/github/andreysum/pfinance/controller/MainController.java
+++ src/main/java/com/github/andreysum/pfinance/controller/MainController.java
@@ -5,9 +5,12 @@ import com.github.andreysum.pfinance.db.schema.CategoryEntity;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.stream.Collectors;
 
 /**
@@ -17,9 +20,11 @@ import java.util.stream.Collectors;
  */
 @Component
 public class MainController {
+    private static final int FIRST_ROW = 0;
     private final CategoryRepo categoryRepo;
     @FXML
-    private TableView<CategoryEntity> savingCategoryTable;
+    private TableView<CategoryEntity> table;
+    private TableView.TableViewSelectionModel<CategoryEntity> selectionModel;
 
     public MainController(CategoryRepo categoryRepo) {
         this.categoryRepo = categoryRepo;
@@ -27,8 +32,30 @@ public class MainController {
 
     @FXML
     public void initialize() {
+        populateTable();
+        setupSelectionModel();
+    }
+
+    private void populateTable() {
         ObservableList<CategoryEntity> categories =
                 categoryRepo.findAll().stream().collect(Collectors.toCollection(FXCollections::observableArrayList));
-        savingCategoryTable.getItems().addAll(categories);
+        table.getItems().addAll(categories);
+    }
+
+    private void setupSelectionModel() {
+        selectionModel = table.getSelectionModel();
+        selectionModel.cellSelectionEnabledProperty().setValue(true);
+        selectionModel.select(FIRST_ROW);
+    }
+
+    @FXML
+    @Transactional
+    protected void onEditCommit(TableColumn.CellEditEvent<CategoryEntity, String> event) {
+        TablePosition<CategoryEntity, String> pos = event.getTablePosition();
+        String newCategoryTitle = event.getNewValue();
+        int row = pos.getRow();
+        CategoryEntity category = event.getTableView().getItems().get(row);
+        category.setTitle(newCategoryTitle);
+        categoryRepo.save(category);
     }
 }
